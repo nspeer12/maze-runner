933310f9c90e85f0832ecaf1ca4a83783414b612
nspeer12 <nspeer252@gmail.com>
2019-11-04 08:27:24 -0500

    makeMazeFile is working beautifully

--- makeMazeFile.py
+++ makeMazeFile.py
@@ -1,5 +1,6 @@
 from random import seed
 from random import random
+import numpy as np
 
 def makeMazeFile(filename, x, y):
 	matrix = []
@@ -9,17 +10,31 @@ def makeMazeFile(filename, x, y):
 	for i in range(x):
 		subMatrix = []
 		for j in range(y):
-			if j == 0:
-				subMatrix.append('# ')
-			elif j == y - 1:
+			if j == 0 or i == 0:
+				subMatrix.append('#')
+			elif j == y - 1 or i == x -1:
 				subMatrix.append('#')
 			else:
 				rand = random()
-				if rand < 0.6:
-					subMatrix.append('# ')
-				elif rand >= 0.6:
-					subMatrix.append('0 ')
+				if hasEntrance == False and rand > 0.9:
+					subMatrix.append('@')
+					hasEntrance = True
+				elif hasExit == False and rand < 0.1:
+					subMatrix.append('e')
+					hasExit = True
+				elif rand < 0.3:
+					subMatrix.append('#')
+				elif rand >= 0.3:
+					subMatrix.append('0')
 
 		matrix.append(subMatrix)
 
-	print(matrix)
+	# write maze to file
+	file = open(filename, 'w')
+	for arr in matrix:
+		for j in range(len(arr)):
+			file.write(arr[j])
+			# add a space except for at the end
+			if (j != len(matrix) - 1):
+				file.write(' ')
+		file.write('\n')
